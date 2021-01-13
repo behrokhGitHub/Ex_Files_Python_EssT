6990c74881260dc098a6eae0c685f02d886364df
Behrokh Farzad <behrokhfarzad@gmail.com>
2021-01-12 16:01:17 -0800

    ch03:types, seq, id()

--- Exercise Files/Chap03/types.py
+++ Exercise Files/Chap03/types.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 7
+from decimal import *
+
+p = Decimal('0.1')
+q = Decimal('0.1')
+r = Decimal('0.1')
+s = Decimal('0.3')
+t = p+q+r-s
+print('p+q-r {}'.format(p+q+r-s))
+print(type(t))
+
+x = 'seven'
+print('seven {} {}'.format(8, 9))
+print('seven {1} {0}'.format(8, 9))
+print('seven {:>9} {:>9}'.format(8, 9))
+print('seven {:>09} {:>09}'.format(8, 9))
+a = 8
+b = 9
+# using fstring performs the same as .format() 
+x = f'seven {9} {9}'
+y = f'seven {a} {b}'
 print('x is {}'.format(x))
+print('y is {}'.format(y))
 print(type(x))
+
