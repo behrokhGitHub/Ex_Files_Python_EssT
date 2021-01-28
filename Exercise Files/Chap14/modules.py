da7b7a7b62633d1c4f7dfb75f5b3ebb344b6934b
Behrokh Farzad <behrokhfarzad@gmail.com>
2021-01-27 18:52:26 -0800

    modules, random.shuffle()

--- Exercise Files/Chap14/modules.py
+++ Exercise Files/Chap14/modules.py
@@ -2,9 +2,28 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 import sys
+import os
+import random
 
 def main():
     v = sys.version_info
     print('Python version {}.{}.{}'.format(*v))
+    
+    p = sys.platform
+    print(p)
+    
+    o = os.name
+    print(o)
+    
+    path = os.getenv('PATH')
+    print(path)
+    
+    cwd = os.getcwd()
+    print(cwd)
+    
+    ran = list(range(25))
+    print(ran)
+    random.shuffle(ran)
+    print(ran)
 
 if __name__ == '__main__': main()
