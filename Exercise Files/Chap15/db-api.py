6b449db80b9c0e8bc27dc8a945b59e4cde7d6d1b
Behrokh Farzad <behrokhfarzad@gmail.com>
2021-01-28 16:45:19 -0800

    creating db and insert a few rows

--- Exercise Files/Chap15/db-api.py
+++ Exercise Files/Chap15/db-api.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-import sqlite3
+import sqlite3 as sq
 
 def main():
     print('connect')
-    db = sqlite3.connect('db-api.db')
+    
+    '''
+    To create a databases using the connect() function of the sqlite3 module.
+    db is the connection object.
+    '''
+    db = sq.connect('db-api.db')
+    
+    '''
+    Create a Cursor object to call its execute() method to perform SQL commands:
+    '''
     cur = db.cursor()
+    
     print('create')
+    
+    '''
+    Executes an SQL statement using execute()
+    
+    DROP TABLE IF EXISTS SQL statement checks if the table exists prior to drop/delete it 
+    '''
     cur.execute("DROP TABLE IF EXISTS test")
+    
+    '''
+    CREATE TABLE() to create a table
+    '''
     cur.execute("""
         CREATE TABLE test (
             id INTEGER PRIMARY KEY, string TEXT, number INTEGER
@@ -27,9 +47,19 @@ def main():
         INSERT INTO test (string, number) VALUES ('three', 3)
         """)
     print('commit')
+    '''
+    to save the table use commit()
+    '''
     db.commit()
+    
     print('count')
+    
+    '''
+    SELECT COUNT find the number of products
+    '''
     cur.execute("SELECT COUNT(*) FROM test")
+    
+    
     count = cur.fetchone()[0]
     print(f'there are {count} rows in the table.')
     print('read')
